e9ce9dc2018a176a6c3a7a9a493c85939d6a5082
Kashslesh <46224990+Kashslesh@users.noreply.github.com>
2021-02-06 21:59:08 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Initialition des variabels
 const spaceship = document.querySelector(".spaceship");
 const shot = document.querySelector(".shot");
-let parSpaceShip = 0;
+let parSpaceShip = 20;
 // creation d'un logique des coups
 function shotGun() {
   if (shot.style.display != "block") {
@@ -11,17 +11,39 @@ function shotGun() {
     shot.style.display = "none";
   }, 300);
 }
-// creation d'un logique du pressage de clavier  
+// creation d'un logique du pressage de clavier
 document.addEventListener("keydown", function (event) {
-if (event.key == 0 || event.key == 32) {
+  if (event.key == 0 || event.key == 32) {
     shotGun();
-}else if ("ArrowRight"){
-    let par = parSpaceShip - 20;
-    spaceship.style.transform = `translateX(${par}px)`
-
-}else if ("ArrowLeft"){
-    let par = parSpaceShip + 20;
-    spaceship.style.transform = `translateX(${par}px)`
-}
+  }
   // console.log(event.key);
 });
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+  e = e || window.event;
+  let parApresPar = 10
+  if (e.keyCode == "37") {
+    console.log("pressed left");
+    if (parSpaceShip<=0){
+      parApresPar = 0
+    }else {
+      parSpaceShip--;
+      spaceship.style.left=`${parApresPar*parSpaceShip}px`;
+      shot.style.left=`${parApresPar*parSpaceShip+5}px`;
+    }
+
+    
+  } else if (e.keyCode == "39") {
+    console.log("pressed right");
+    if (parSpaceShip>=20){
+      parSpaceShip = 20
+    }else {
+      parSpaceShip++;
+      spaceship.style.left=`${parApresPar*parSpaceShip}px`;
+      shot.style.left=`${parApresPar*parSpaceShip+5}px`;
+    }
+  }
+}
+
